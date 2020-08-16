436a523108cd7c7c72a8f3cb6d9a9049c679f728
Mareesh Kumar Issar <mareesh.issar@rutgers.edu>
2020-08-16 02:07:44 -0400

    final version

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,9 @@
 const mongodb = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 
-//const mongoURL = 'mongodb://localhost:27017/emojifier';
+const mongoURL = 'mongodb://localhost:27017/emojifier';
 
-const mongoURL = 'add your mongoDB Atlas database link here';
+//const mongoURL = 'add your mongoDB Atlas database link here';
 
 module.exports = function saveFace(imageUrl, data) {
   MongoClient.connect(mongoURL, { useNewUrlParser: true }, (err, db) => {
